bb8bfa1c6f0fe2c38402340de4f6319aaa9ae219
Mohammad Amin Ahmadi <en.mmdamin@gmail.com>
2024-05-03 11:42:40 +0330

    create vite react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="">
+      <div className="flex absolute items-end justify-between gap-3 bg-blue-400 md:bg-red-600 w-[400px] h-[600px]">
+        <div>
+          <div className="size-10 bg-fuchsia-700"></div>
+          <div className="size-10 bg-fuchsia-700"></div>
+          <div className="size-10 bg-fuchsia-700 flex-1"></div>
+        </div>
+        <div className="size-10 bg-[#6C32D2]"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import classes from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={classes.container}>Header</div>
+  )
+}
+
+export default Header
